2dfcd6f36a2092a289bd82037d886b5e46be2cc8
Eddie Machado <eddie@tapiture.com>
2013-12-27 15:15:21 -0500

    added better title tag function
    http://www.deluxeblogtips.com/2012/03/better-title-meta-tag.html

--- library/bones.php
+++ library/bones.php
@@ -58,6 +58,37 @@ function bones_head_cleanup() {
 
 } /* end bones head cleanup */
 
+// A better title
+// http://www.deluxeblogtips.com/2012/03/better-title-meta-tag.html
+function rw_title( $title, $sep, $seplocation ) {
+  global $page, $paged;
+
+  // Don't affect in feeds.
+  if ( is_feed() ) return $title;
+
+  // Add the blog's name
+  if ( 'right' == $seplocation ) {
+    $title .= get_bloginfo( 'name' );
+  } else {
+    $title = get_bloginfo( 'name' ) . $title;
+  }
+
+  // Add the blog description for the home/front page.
+  $site_description = get_bloginfo( 'description', 'display' );
+
+  if ( $site_description && ( is_home() || is_front_page() ) ) {
+    $title .= " {$sep} {$site_description}";
+  }
+
+  // Add a page number if necessary:
+  if ( $paged >= 2 || $page >= 2 ) {
+    $title .= " {$sep} " . sprintf( __( 'Page %s', 'dbt' ), max( $paged, $page ) );
+  }
+
+  return $title;
+
+} // end better title
+
 // remove WP version from RSS
 function bones_rss_version() { return ''; }
 
