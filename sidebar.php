cc03b2831ad122dbd0201042765f13baa87bf028
andamira <andamira@users.noreply.github.com>
2014-02-20 01:29:36 +0100

    Update sidebar.php
    Added missing class t-1of3 to match t-2of3 in the main container, for tablet sizes.

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-				<div id="sidebar1" class="sidebar m-all d-2of7 last-col cf" role="complementary">
+				<div id="sidebar1" class="sidebar m-all t-1of3 d-2of7 last-col cf" role="complementary">
 
 					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
