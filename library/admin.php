e4d82364f47b3cdd0f1978ad15ade519bcbd4bcd
Eddie Machado <eddie@tapiture.com>
2014-05-08 22:50:12 -0700

    changed link to wp candy until themble is back online

--- library/admin.php
+++ library/admin.php
@@ -62,7 +62,7 @@ http://digwp.com/2010/10/customize-wordpress-dashboard/
 function bones_rss_dashboard_widget() {
 	if ( function_exists( 'fetch_feed' ) ) {
 		// include_once( ABSPATH . WPINC . '/feed.php' );               // include the required file
-		$feed = fetch_feed( 'http://themble.com/feed/rss/' );        // specify the source feed
+		$feed = fetch_feed( 'http://feeds.feedburner.com/wpcandy' );        // specify the source feed
 		$limit = $feed->get_item_quantity(7);                        // specify number of items
 		$items = $feed->get_items(0, $limit);                        // create an array of items
 	}
