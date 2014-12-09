641e0bdc7c7a0f19a2caea4453188d24ff3424da
Eddie Machado <eddie.machado@Eddie-Machados-Macbook-Pro-2.local>
2014-12-09 10:12:14 -0600

    added support for Chrome theme color
    more here:
    http://updates.html5rocks.com/2014/11/Support-for-theme-color-in-Chrome-
    39-for-Android

--- header.php
+++ header.php
@@ -27,6 +27,7 @@
 		<?php // or, set /favicon.ico for IE10 win ?>
 		<meta name="msapplication-TileColor" content="#f01d4f">
 		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+            <meta name="theme-color" content="#121212">
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
