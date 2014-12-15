9a8e6f2b05d93bb756e8d64df98e4044866c2a9d
José Luis Cruz <joseluis@users.noreply.github.com>
2014-12-15 23:33:42 +0100

    missing semicolon
    issue #698

--- library/js/scripts.js
+++ library/js/scripts.js
@@ -20,7 +20,7 @@
 */
 function updateViewportDimensions() {
 	var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
-	return { width:x,height:y }
+	return { width:x,height:y };
 }
 // setting the viewport width
 var viewport = updateViewportDimensions();
