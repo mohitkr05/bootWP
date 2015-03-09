f1b55117f2d03c62004b5cdce518628ced7c3f95
RLGyde <gyde@gyde.dk>
2015-03-09 19:26:34 +0100

    Coretted dubbel by in byline. Devided byline and archived under to that translation works.

--- post-formats/format-link.php
+++ post-formats/format-link.php
@@ -7,11 +7,11 @@
                   <h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
 
                   <p class="byline vcard">
-                    <?php printf( __( 'Posted %1$s by %2$s', 'bonestheme' ),
+                    <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
                        /* the time the post was published */
                        '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
                        /* the author of the post */
-                       '<span class="by">by</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
+                       '<span class="by">'.__( 'by', 'bonestheme' ).'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
                     ); ?>
                   </p>
 
@@ -51,6 +51,6 @@
 
                 </footer> <?php // end article footer ?>
 
-                <?php comments_template(); ?>
+                <?php //comments_template(); ?>
 
               </article> <?php // end article ?>
